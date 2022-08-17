2a7eed410a3f49959b5fa5de200f42cb10cb522d
Pedro Borges <p.borges.17@ucl.ac.uk>
2022-08-17 17:05:29 +0100

    Saving more frequently

--- trainTT.py
+++ trainTT.py
@@ -92,20 +92,19 @@ class trainTT:
                     loss_PAF += self.PAF_torch_loss(batch_paf, paf_out)
 
                     # Save outputs once every epoch
-                    if (iteration == 0) and (ii == (len(heatmap_outputs) - 1)):
+                    if (iteration % 100 == 0) and (ii == (len(heatmap_outputs) - 1)):
                         # Save output heatmap and paf
                         val_saver(heatmap_out.max(axis=1)[0].squeeze().cpu().detach().numpy(),
                                   affine, self.figures_dir, "Train_heatmap", epoch, ii)
                         val_saver(paf_out.sum(axis=-1).squeeze().permute(3, 1, 2, 0).cpu().detach().numpy(),
                                   affine, self.figures_dir, "Train_paf", epoch, ii)
-                        if ii == (len(heatmap_outputs) - 1):
-                            # Save OGs
-                            val_saver(batch_heatmap.max(axis=1)[0].squeeze().cpu().detach().numpy(),
-                                      affine, self.figures_dir, "Train_GT_heatmap", epoch, None)
-                            val_saver(batch_paf.sum(axis=-1).squeeze().cpu().permute(3, 1, 2, 0).detach().numpy(),
-                                      affine, self.figures_dir, "Train_GT_paf", epoch, None)
-                            val_saver(batch_image.squeeze().cpu().detach().numpy(),
-                                      affine, self.figures_dir, "Train_Image", epoch, None)
+                        # Save OGs
+                        val_saver(batch_heatmap.max(axis=1)[0].squeeze().cpu().detach().numpy(),
+                                  affine, self.figures_dir, "Train_GT_heatmap", epoch, None)
+                        val_saver(batch_paf.sum(axis=-1).squeeze().cpu().permute(3, 1, 2, 0).detach().numpy(),
+                                  affine, self.figures_dir, "Train_GT_paf", epoch, None)
+                        val_saver(batch_image.squeeze().cpu().detach().numpy(),
+                                  affine, self.figures_dir, "Train_Image", epoch, None)
 
                     del heatmap_out, paf_out
 
